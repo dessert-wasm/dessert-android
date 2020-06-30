7f8922919e8a1401a6ecf817f566a31eda368418
Renaud <renaud.melet@epitech.eu>
2020-06-30 19:03:11 +0200

    Handle async auth

--- app/src/main/java/com/dessert/ui/login/LoginActivity.kt
+++ app/src/main/java/com/dessert/ui/login/LoginActivity.kt
@@ -1,6 +1,7 @@
 package com.dessert.ui.login
 
 import android.app.Activity
+import android.content.Intent
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
@@ -16,6 +17,9 @@ import android.widget.ProgressBar
 import android.widget.Toast
 
 import com.dessert.R
+import com.dessert.data.model.LoggedInUser
+import com.dessert.ui.dashboard.DashboardFragment
+import com.dessert.ui.home.HomeFragment
 
 class LoginActivity : AppCompatActivity() {
 
@@ -31,8 +35,8 @@ class LoginActivity : AppCompatActivity() {
         val login = findViewById<Button>(R.id.login)
         val loading = findViewById<ProgressBar>(R.id.loading)
 
-        loginViewModel = ViewModelProviders.of(this, LoginViewModelFactory())
-                .get(LoginViewModel::class.java)
+        loginViewModel =
+            ViewModelProviders.of(this, LoginViewModelFactory()).get(LoginViewModel::class.java)
 
         loginViewModel.loginFormState.observe(this@LoginActivity, Observer {
             val loginState = it ?: return@Observer
@@ -57,35 +61,38 @@ class LoginActivity : AppCompatActivity() {
             }
             if (loginResult.success != null) {
                 updateUiWithUser(loginResult.success)
+                setResult(Activity.RESULT_OK)
+                /*val intent = Intent(this, DashboardFragment::class.java).apply {
+                    putExtra("id", loginResult.success.userId)
+                }
+                startActivity(intent)*/
+                //Complete and destroy login activity once successful
+                finish()
             }
-            setResult(Activity.RESULT_OK)
-
-            //Complete and destroy login activity once successful
-            finish()
         })
 
         username.afterTextChanged {
             loginViewModel.loginDataChanged(
-                    username.text.toString(),
-                    password.text.toString()
+                username.text.toString(),
+                password.text.toString()
             )
         }
 
         password.apply {
             afterTextChanged {
                 loginViewModel.loginDataChanged(
-                        username.text.toString(),
-                        password.text.toString()
+                    username.text.toString(),
+                    password.text.toString()
                 )
             }
 
             setOnEditorActionListener { _, actionId, _ ->
                 when (actionId) {
-                    EditorInfo.IME_ACTION_DONE ->
-                        loginViewModel.login(
-                                username.text.toString(),
-                                password.text.toString()
-                        )
+                    EditorInfo.IME_ACTION_DONE -> {
+                        loading.visibility = View.VISIBLE
+                        loginViewModel.login(username.text.toString(), password.text.toString())
+                    }
+
                 }
                 false
             }
@@ -97,14 +104,14 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-    private fun updateUiWithUser(model: LoggedInUserView) {
+    private fun updateUiWithUser(model: LoggedInUser) {
         val welcome = getString(R.string.welcome)
         val displayName = model.displayName
         // TODO : initiate successful logged in experience
         Toast.makeText(
-                applicationContext,
-                "$welcome $displayName",
-                Toast.LENGTH_LONG
+            applicationContext,
+            "$welcome $displayName",
+            Toast.LENGTH_LONG
         ).show()
     }
 
--- app/src/main/java/com/dessert/ui/login/LoginResult.kt
+++ app/src/main/java/com/dessert/ui/login/LoginResult.kt
@@ -1,9 +1,11 @@
 package com.dessert.ui.login
 
+import com.dessert.data.model.LoggedInUser
+
 /**
  * Authentication result : success (user details) or error message.
  */
 data class LoginResult(
-        val success: LoggedInUserView? = null,
+        val success: LoggedInUser? = null,
         val error: Int? = null
 )
