2404197c6796a2b2256145ad6515803fbca86251
Cecilia <cecilia.jiang@epitech.eu>
2020-09-06 16:19:59 +0200

    [ADD] Home : my modules

--- app/src/main/java/com/dessert/ui/home/HomeFragment.kt
+++ app/src/main/java/com/dessert/ui/home/HomeFragment.kt
@@ -1,31 +1,50 @@
 package com.dessert.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.coroutines.toDeferred
+import com.apollographql.apollo.exception.ApolloException
+import com.apollographql.apollo.sample.UserQuery
+import com.apollographql.apollo.sample.type.PaginationQueryInput
 import com.dessert.R
 
 class HomeFragment : Fragment() {
-
-    private lateinit var homeViewModel: HomeViewModel
+    val apolloClient = ApolloClient.builder().serverUrl("https://dev.dessert.vodka").build()
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        val modulesRecyclerView = root.findViewById<RecyclerView>(R.id.modules_list)
+        modulesRecyclerView.layoutManager = LinearLayoutManager(context)
+
+        val id = requireActivity().intent.extras!!.getInt("ID");
+
+        lifecycleScope.launchWhenResumed {
+            val response = try {
+                apolloClient.query(UserQuery(id, PaginationQueryInput(true, 0, 50))).toDeferred()
+                    .await()
+            } catch (e: ApolloException) {
+                Log.i("Home", "Failure", e)
+                null
+            }
+            modulesRecyclerView.adapter = HomeDescAdapter(response!!.data!!.user.modules.result!!)
+
+        }
+//        val textView: TextView = root.findViewById(R.id.text_home)
+//        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
         return root
     }
 }
--- app/src/main/java/com/dessert/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/dessert/ui/home/HomeViewModel.kt
@@ -1,13 +1,51 @@
 package com.dessert.ui.home
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.apollographql.apollo.sample.UserQuery
+import com.dessert.R
+import me.gujun.android.taggroup.TagGroup
+import java.text.SimpleDateFormat
 
-class HomeViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+class HomeDescAdapter(val results: List<UserQuery.Result?>) : RecyclerView.Adapter<HomeDescAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeDescAdapter.ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val row = layoutInflater.inflate(R.layout.fragment_modules_description, parent, false)
+
+        return ViewHolder(row)
     }
-    val text: LiveData<String> = _text
+
+    override fun getItemCount(): Int {
+        return results.size
+    }
+
+    override fun onBindViewHolder(holder: HomeDescAdapter.ViewHolder, position: Int) {
+        val result = results.get(position)
+        val sdf = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+        val date = sdf.parse(result!!.publishedDateTime.toString())
+
+        sdf.applyPattern("yyyy-MM-dd");
+
+        holder.profileView.findViewById<TextView>(R.id.module_name).text = result.name;
+        holder.profileView.findViewById<TextView>(R.id.module_date).text = sdf.format(date);
+        holder.profileView.findViewById<TextView>(R.id.module_desc).text = result.description;
+        holder.profileView.findViewById<TextView>(R.id.module_author).text = "@" + result!!.author.nickname;
+
+        if (result.tags.size > 0) {
+            val list = mutableListOf<String>()
+            val tagGroup = holder.profileView.findViewById<TagGroup>(R.id.tag_group)
+
+            for (tag in result.tags) {
+                list.add(tag.name)
+            }
+            tagGroup.setTags(list)
+        }
+    }
+
+    inner class ViewHolder(val profileView: View) : RecyclerView.ViewHolder(profileView)
 }
+
