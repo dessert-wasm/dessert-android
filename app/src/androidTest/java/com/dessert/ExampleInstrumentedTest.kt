6aa85464ef4ad7cc1ae0313aca2e2a33ecccb8bb
Reno <renaud.melet@sp3d.co>
2020-11-05 14:02:56 +0100

    Add tests

--- app/src/androidTest/java/com/dessert/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/dessert/ExampleInstrumentedTest.kt
@@ -1,7 +1,17 @@
 package com.dessert
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import org.junit.Before
+import org.junit.Rule
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+
+
+import androidx.test.platform.app.InstrumentationRegistry
+import com.dessert.ui.login.LoginActivity
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -20,5 +30,37 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.dessert", appContext.packageName)
+    }
+}
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class ChangeTextBehaviorTest {
+
+    private lateinit var stringToBetyped: String
+
+    @get:Rule
+    var activityRule: ActivityScenarioRule<LoginActivity> = ActivityScenarioRule(LoginActivity::class.java)
+
+    @Before
+    fun initValidString() {
+        // Specify a valid string.
+        stringToBetyped = "Espresso"
+    }
+
+    @Test
+    fun changeText_sameActivity() {
+        // Type text and then press the button.
+        onView(withId(R.id.username)).perform(typeText("lucas.santoni@epitech.eu"), closeSoftKeyboard());
+        onView(withId(R.id.password)).perform(typeText("12345"), closeSoftKeyboard());
+        onView(withId(R.id.login)).perform(click());
+
+        /*onView(withId(R.id.editTextUserInput))
+            .perform(typeText(stringToBetyped), closeSoftKeyboard())
+        onView(withId(R.id.changeTextBt)).perform(click())
+
+        // Check that the text was changed.
+        onView(withId(R.id.textToBeChanged))
+            .check(matches(withText(stringToBetyped)))*/
     }
 }
--- app/src/test/java/com/dessert/data/LoginTest.kt
+++ app/src/test/java/com/dessert/data/LoginTest.kt
@@ -5,8 +5,10 @@ import com.apollographql.apollo.ApolloClient
 import com.apollographql.apollo.api.Response
 import com.apollographql.apollo.exception.ApolloException
 import com.apollographql.apollo.sample.LoginMutation
+import com.apollographql.apollo.sample.UserQuery
+import com.apollographql.apollo.sample.type.PaginationQueryInput
 import com.dessert.CustomApolloClient
-import org.junit.Assert
+import org.junit.Assert.*
 import org.junit.Test
 
 class LoginTest {
@@ -14,12 +16,33 @@ class LoginTest {
     fun login() {
         val client: ApolloClient = CustomApolloClient.client
 
-        Assert.assertEquals(true, client.serverUrl.isHttps)
-        Assert.assertEquals("prod.dessert.vodka", client.serverUrl.host())
+        assertEquals(true, client.serverUrl.isHttps)
+        assertEquals("prod.dessert.vodka", client.serverUrl.host())
 
         client.mutate(LoginMutation("lucas.santoni@epitech.eu", "12345", true))
             .enqueue(object : ApolloCall.Callback<LoginMutation.Data>() {
                 override fun onResponse(response: Response<LoginMutation.Data>) {
+                    val id = response.data!!.login.id;
+
+                    assertEquals(true, id > 0)
+
+                    CustomApolloClient.client.query(
+                        UserQuery(
+                            id,
+                            PaginationQueryInput(true, 0, 20)
+                        )
+                    )
+                        .enqueue(object : ApolloCall.Callback<UserQuery.Data>() {
+                            override fun onFailure(e: ApolloException) {
+                                assert(true)
+                            }
+
+                            override fun onResponse(response: Response<UserQuery.Data>) {
+                                assertEquals("Lucas", response.data?.user?.nickname)
+                                assert(response.data?.user?.profilePicUrl.toString().isEmpty())
+                            }
+                        })
+
 
                 }
 
